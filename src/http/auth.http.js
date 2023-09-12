2dd34923ec24c5e9d71d8afb11f08d270dc715bb
TheDusty117 <gabrieledipassio@gmail.com>
2023-09-12 19:16:04 +0200

    FEAT add correct APIs to login, singup and logout

--- src/http/auth.http.js
+++ src/http/auth.http.js
@@ -1,19 +1,18 @@
-import axios from "./";
-
+import axios from './'
 
 const AuthAPI = {
     loginUser: data => {
-        const url = '/auth/login'
+        const url = 'https://todoapp.backend.k-stage.dev/api/auth/login'
         return axios.post(url, data)
     },
     logoutUser: () => {
-        const url = '/auth/logout'
+        const url = 'https://todoapp.backend.k-stage.dev/api/auth/logout'
         return axios.get(url)
     },
     signupUser: data => {
-        const url = '/auth/signup'
+        const url = 'https://todoapp.backend.k-stage.dev/api/auth/signup'
         return axios.post(url, data)
-    }
+    },
 }
 
 export default AuthAPI
